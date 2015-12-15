94f2162117881ba882640c0f39c5055f43871401
Caroline Boström <edane.1986@gmail.com>
2015-12-15 12:01:07 +0100

    added packages

--- init.el
+++ init.el
@@ -30,6 +30,8 @@
 ;;load use-package
 (require 'use-package)
 
+;;install php-mode
+(use-package php-mode :ensure php-mode)
 
 ;;install guide-key
 (use-package guide-key :ensure guide-key)
@@ -39,6 +41,15 @@
 
 ;;load guide-key
 (guide-key-mode 1)
+
+;;install web-mode
+(use-package web-mode :ensure web-mode)
+
+;;config web-mode
+:config (progn
+		  (setq-default web-mode-markup-indent-offset 4) ;;html
+		  (setq-default web-mode-css-indent-offset 4) ;;css
+		  (setq-default web-mode-code-indent-offset 4));;PHP/JS/etc
 
 
 ;; install auto-complete
